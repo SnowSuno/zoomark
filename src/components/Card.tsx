75ddb0d111175052780d4da9430406951345077e
SnowSuno <snowsuno.dev@gmail.com>
2022-03-08 15:21:54 +0900

    feat : add components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef, useState, useEffect} from "react";
 import styles from "./Card.module.scss";
 
 
@@ -10,15 +10,49 @@ interface CardProps {
 }
 
 function Card({meeting}: CardProps) {
+    const [showMenu, setShowMenu] = useState<boolean>(false);
+    const menuRef = useRef<HTMLDivElement>(null);
 
+    useEffect(() => {
+    const handleClickOutside =(event: any) => {
+      if (!menuRef.current?.contains(event.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef, setShowMenu]);
 
     return (
         <div
             className={styles.card}
-            onClick={() => console.log("clicked")}
+
         >
-            <h1>{meeting.name}</h1>
-            <p>201 223 4203</p>
+            <div
+                className={styles.clickable}
+                onClick={() => console.log("clicked")}
+            >
+                <h1>{meeting.name}</h1>
+                <p>201 223 4203</p>
+            </div>
+
+
+            <button
+                className={styles.button}
+                onClick={(e) => {
+                    e.stopPropagation();
+                    setShowMenu(true);
+                }}
+            >
+                <p>&middot;&middot;&middot;</p>
+            </button>
+            {showMenu && <div className={styles.menu} ref={menuRef}>
+                <p>수정</p>
+                <p>삭제</p>
+            </div>}
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import styles from "./Header.module.scss";
+import classNames from "classnames";
+import logo from "../logo.png";
 
 function Header() {
 
 
     return (
         <div className={styles.header}>
-
+            <span>
+                <img src={logo} alt="logo"/>
+                <h1>Zoom Pin</h1>
+            </span>
+            <div className={styles.toolBar}>
+                <input
+                    className={classNames(styles.tool)}
+                    type="text"
+                />
+                <button
+                    className={classNames(styles.tool)}
+                >
+                    +
+                </button>
+            </div>
         </div>
     )
 }
